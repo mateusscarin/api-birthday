45731f128c3e2bbafcd346a96eaff1fe59d81abb
Mateus-Scarin <mateus.scarin@gmail.com>
2023-02-26 18:46:27 -0300

    feat: Initial commit :tada:

---
+++ src/main/java/com/br/apibirthday/ApiBirthdayApplication.java
@@ -0,0 +1,13 @@
+package com.br.apibirthday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBirthdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBirthdayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/apibirthday/ApiBirthdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.apibirthday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBirthdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
