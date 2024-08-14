478e68dbca822b45e931cf95d413767db68bed22
mateusscarin <mateus.scarin@gmail.com>
2024-08-14 19:59:47 -0300

    style: ajuste de diretório

--- src/main/java/com/br/birthday/ApiBirthdayApplication.java
+++ src/main/java/com/br/birthday/ApiBirthdayApplication.java
@@ -1,4 +1,4 @@
-package com.br.apibirthday;
+package com.br.birthday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/br/birthday/model/Birthday.java
+++ src/main/java/com/br/birthday/model/Birthday.java
@@ -1,4 +1,4 @@
-package com.br.apibirthday.model;
+package com.br.birthday.model;
 
 import java.io.Serializable;
 import java.time.LocalDate;
